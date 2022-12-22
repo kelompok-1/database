25ab733236f791fb8e25f73d8daac244f87d357c
alifimn <99638740+alifimn@users.noreply.github.com>
2022-12-22 19:11:03 +0700

    update database

--- skoolen.sql
+++ skoolen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2022 at 02:27 AM
+-- Generation Time: Dec 22, 2022 at 11:59 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `skoolen`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audio`
+--
+
+CREATE TABLE `audio` (
+  `nama_audio` varchar(45) NOT NULL,
+  `nama_author` varchar(45) NOT NULL,
+  `gambar` varchar(225) NOT NULL,
+  `link_audio` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `audio`
+--
+
+INSERT INTO `audio` (`nama_audio`, `nama_author`, `gambar`, `link_audio`) VALUES
+('Audio Test 1', 'Muhammad Cakra', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fgambar%2Fcircle-blue-number-1-png-12.png?alt=media&token=16d93809-f7f8-4528-a8af-b0a17063837c', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fy2mate.com%20-%20Run%20%20Sound%20Effect%20Perfect%20Cut.mp3?alt=media&token=057d3414-be9f-43c8-85e9-b3beb70d04fb'),
+('Audio Test 2', 'Aiko Mawardi', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fgambar%2F1024px-Number_2_in_light_blue_rounded_square.svg.png?alt=media&token=1913336b-5b8c-41c2-af8c-1be35b810eb0', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fdl159.y2mate.com.mp3?alt=media&token=cf81be5c-a835-42c8-a628-6368af1336ad'),
+('Audio Test 3', 'Kristian Fransicus', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fgambar%2FUnknown.jpeg?alt=media&token=c369e98b-202d-4613-95d0-7c861c3c534a', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/audio%2Fy2mate.com%20-%2030%20Second%20Audio%20Intro.mp3?alt=media&token=c8d06442-fa17-4d6a-aac2-24cdf3618d5b');
 
 -- --------------------------------------------------------
 
@@ -85,11 +107,13 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`username`, `email`, `password`, `level`) VALUES
-('admin', 'admin@admin.com', 'admin', '1'),
-('aikomawardiiiii', 'aikomawardii@gmail.com', 'aremafire778', '2'),
-('alifimanullah', 'alifimanullah12@gmail.com', '123456aa', '2'),
+('admin', 'admin@skoolen.com', 'admin', '1'),
+('aikomawardi', 'aikomawardi@gmail.com', 'aremafire778', '2'),
+('aikomawardi778', 'aikomawardi@gmail.com', '123456', '2'),
+('alifimanullah', 'alifimanullah@gmail.com', '123456', '2'),
 ('cakranew', 'muhammadcakranew@gmail.com', '123456', '2'),
-('ikashi', 'ikashiamv@gmail.com', 'akbar123', '2');
+('lalala', 'lala@gmail.com', '123456', '2'),
+('nania', 'nania@gmail.com', '123456', '2');
 
 -- --------------------------------------------------------
 
@@ -129,22 +153,29 @@ CREATE TABLE `video` (
   `nama_video` varchar(45) NOT NULL,
   `durasi` varchar(45) NOT NULL,
   `link_video` varchar(500) NOT NULL,
-  `nama_sub_kategori_video` varchar(45) NOT NULL
+  `nama_sub_kategori_video` varchar(45) NOT NULL,
+  `thumbnail` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `video`
 --
 
-INSERT INTO `video` (`urutan`, `nama_video`, `durasi`, `link_video`, `nama_sub_kategori_video`) VALUES
-('3', 'Face Workout', '0 min, 58 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fdl29.y2mate.com.mp4?alt=media&token=79201f7a-6203-4ca4-ba2d-a14c5ae0dc4e', 'How To Speak Fast'),
-('1', 'The \"R\" Sound', '1 min, 06 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fy2mate.com%20-%201%20minute%20video%20introduction%20%20Ceejay%20Rain%20%20Freelancer_360p.mp4?alt=media&token=cf0bc485-f590-42ba-9a7f-4472052959b8', 'Pronunciation Practice'),
-('2', 'The \"T\" Sound', '1 min, 8 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fy2mate.com%20-%20SELFINTRODUCTION%20VIDEO%20%201%20MINUTE_360p.mp4?alt=media&token=d633a78c-351e-4487-847a-aaa4005f0c8e', 'Pronunciation Practice'),
-('4', 'warm up', '0 min, 30 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fdl257.y2mate.com.mp4?alt=media&token=386c7869-3a10-4f36-a399-8a0f9673c05c', 'Listening Words');
+INSERT INTO `video` (`urutan`, `nama_video`, `durasi`, `link_video`, `nama_sub_kategori_video`, `thumbnail`) VALUES
+('3', 'Face Workout', '0 min, 58 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fdl29.y2mate.com.mp4?alt=media&token=79201f7a-6203-4ca4-ba2d-a14c5ae0dc4e', 'How To Speak Fast', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fthumbnail_video%2FScreenshot%202022-12-22%20at%2016.59.51.png?alt=media&token=652e961d-9940-483e-9f11-bf50629a6ef9'),
+('1', 'The \"R\" Sound', '1 min, 06 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fy2mate.com%20-%201%20minute%20video%20introduction%20%20Ceejay%20Rain%20%20Freelancer_360p.mp4?alt=media&token=cf0bc485-f590-42ba-9a7f-4472052959b8', 'Pronunciation Practice', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fthumbnail_video%2FScreenshot%202022-12-22%20at%2016.59.37.png?alt=media&token=9046cee0-0b0f-4823-ace9-06a8157bac49'),
+('2', 'The \"T\" Sound', '1 min, 8 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fy2mate.com%20-%20SELFINTRODUCTION%20VIDEO%20%201%20MINUTE_360p.mp4?alt=media&token=d633a78c-351e-4487-847a-aaa4005f0c8e', 'Pronunciation Practice', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fthumbnail_video%2FScreenshot%202022-12-22%20at%2016.59.43.png?alt=media&token=7cb05897-46a5-4fa4-b85d-0afd44324963'),
+('4', 'warm up', '0 min, 30 det', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fdetail_video%2Fdl257.y2mate.com.mp4?alt=media&token=386c7869-3a10-4f36-a399-8a0f9673c05c', 'Listening Words', 'https://firebasestorage.googleapis.com/v0/b/skoolen-stable.appspot.com/o/video%2Fthumbnail_video%2FScreenshot%202022-12-22%20at%2016.59.57.png?alt=media&token=a31a75d4-bcd6-4903-b6f9-7b2d472d28be');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `audio`
+--
+ALTER TABLE `audio`
+  ADD PRIMARY KEY (`nama_audio`);
 
 --
 -- Indexes for table `buku`
